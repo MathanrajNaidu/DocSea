82cf7914cf52b26dc0fc52255805fe31f7f05a4e
Mathanraj Naidu <mathanxp2000@gmail.com>
2019-11-07 10:36:13 +0800

    Swagger in view

---
+++ Controllers/SwaggerViewController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DocSea.Controllers
+{
+    public class SwaggerViewController : Controller
+    {
+        // GET: SwaggerView
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
