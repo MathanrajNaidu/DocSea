c456a1778ce2e922f91a91646f5db5a523ed5622
Mathanraj Naidu <mathanxp2000@gmail.com>
2019-11-07 14:59:17 +0800

    Use Username to login and set default user login

--- Migrations/Configuration.cs
+++ Migrations/Configuration.cs
@@ -1,6 +1,10 @@
 namespace DocSea.Migrations
 {
+    using DocSea.Models;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
     using System;
+    using System.Collections.Generic;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
@@ -18,6 +22,34 @@ namespace DocSea.Migrations
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
             //  to avoid creating duplicate seed data.
+
+            var roleStore = new RoleStore<IdentityRole>(context);
+            var roleManager = new RoleManager<IdentityRole>(roleStore);
+            // RoleTypes is a class containing constant string values for different roles
+            List<IdentityRole> identityRoles = new List<IdentityRole>();
+            identityRoles.Add(new IdentityRole() { Name = RoleTypes.Admin });
+
+            foreach (IdentityRole role in identityRoles)
+            {
+                if(!roleManager.RoleExists(role.Name))
+                    roleManager.Create(role);
+            }
+
+            // Initialize default user
+            var userStore = new UserStore<ApplicationUser>(context);
+            var userManager = new UserManager<ApplicationUser>(userStore);
+            ApplicationUser admin = new ApplicationUser();
+            admin.Email = "admin@admin.com";
+            admin.UserName = "admin";
+
+            if (userManager.FindByName(admin.UserName) == null)
+            {
+                userManager.Create(admin, "Petronas@1");
+                userManager.AddToRole(admin.Id, RoleTypes.Admin);
+            }
+            // Add code to initialize context tables
+
+            base.Seed(context);
         }
     }
 }
--- Models/AccountViewModels.cs
+++ Models/AccountViewModels.cs
@@ -49,9 +49,8 @@ namespace DocSea.Models
     public class LoginViewModel
     {
         [Required]
-        [Display(Name = "Email")]
-        [EmailAddress]
-        public string Email { get; set; }
+        [Display(Name = "User Name")]
+        public string UserName { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
@@ -68,6 +67,10 @@ namespace DocSea.Models
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "User Name")]
+        public string UserName { get; set; }
 
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
@@ -108,5 +111,9 @@ namespace DocSea.Models
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
+    }
+
+    public class RoleTypes {
+        public const string Admin = "Admin";
     }
 }
